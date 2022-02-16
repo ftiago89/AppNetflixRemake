9c5206c8a4b070728e0cd526a5ec03350b6bdc2c
Felipe <ftiagomelo89@gmail.com>
2022-02-16 07:41:20 -0300

    Adicionando Lombok ao projeto e primeiras classes de models.

---
+++ app/src/main/java/com/felipe/netflixremake/model/Category.java
@@ -0,0 +1,14 @@
+package com.felipe.netflixremake.model;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+
+    private String name;
+    private List<Movie> movies;
+}
---
+++ app/src/main/java/com/felipe/netflixremake/model/Movie.java
@@ -0,0 +1,11 @@
+package com.felipe.netflixremake.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Movie {
+
+    private int coverUrl;
+}
