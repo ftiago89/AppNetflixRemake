670413eb767aa2d215fcf86957aa4aa8377128af
Felipe <ftiagomelo89@gmail.com>
2022-02-22 06:58:22 -0300

    Adicionando icone de play e titulo de filme na tela de detalhes de filme.

--- app/src/main/java/com/felipe/netflixremake/MovieActivity.java
+++ app/src/main/java/com/felipe/netflixremake/MovieActivity.java
@@ -2,8 +2,12 @@ package com.felipe.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.Objects;
 
@@ -20,6 +24,15 @@ public class MovieActivity extends AppCompatActivity {
         if (toolbar != null) {
             Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        }
+
+        LayerDrawable layerDrawable =
+                (LayerDrawable) ContextCompat.getDrawable(this,R.drawable.movie_shadows);
+
+        if (layerDrawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+            layerDrawable.setDrawableByLayerId(R.id.drawable_layer_list_image, movieCover);
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(layerDrawable);
         }
     }
 }
