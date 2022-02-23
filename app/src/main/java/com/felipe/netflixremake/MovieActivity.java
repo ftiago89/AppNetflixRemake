ebd0148978c552ceaf714dcf7f260e61845460bf
Felipe <ftiagomelo89@gmail.com>
2022-02-23 06:54:51 -0300

    Adicionando scroll e recycler nas opcoes semelhantes da tela de detalhes de filme.

--- app/src/main/java/com/felipe/netflixremake/MovieActivity.java
+++ app/src/main/java/com/felipe/netflixremake/MovieActivity.java
@@ -1,22 +1,35 @@
 package com.felipe.netflixremake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.felipe.netflixremake.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        recyclerView = findViewById(R.id.recycler_view_similar);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,15 +37,54 @@ public class MovieActivity extends AppCompatActivity {
         if (toolbar != null) {
             Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+            getSupportActionBar().setTitle(null);
         }
 
         LayerDrawable layerDrawable =
                 (LayerDrawable) ContextCompat.getDrawable(this,R.drawable.movie_shadows);
 
-        if (layerDrawable != null) {
-            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
-            layerDrawable.setDrawableByLayerId(R.id.drawable_layer_list_image, movieCover);
-            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(layerDrawable);
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            Movie movie = new Movie();
+            movies.add(movie);
+        }
+
+        recyclerView.setAdapter(new MovieAdapter(movies));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+    }
+
+    private static class MovieHolder extends RecyclerView.ViewHolder {
+
+        final ImageView imageViewCover;
+
+        public MovieHolder(@NonNull View itemView) {
+            super(itemView);
+            imageViewCover = itemView.findViewById(R.id.image_view_cover);
+        }
+    }
+
+    private class MovieAdapter extends RecyclerView.Adapter<MovieActivity.MovieHolder> {
+
+        private final List<Movie> movies;
+
+        public MovieAdapter(List<Movie> movies) {
+            this.movies = movies;
+        }
+
+        @NonNull
+        @Override
+        public MovieActivity.MovieHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new MovieActivity.MovieHolder(getLayoutInflater().inflate(R.layout.movie_item_similar, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MovieActivity.MovieHolder holder, int position) {
+            Movie movie = movies.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return movies.size();
         }
     }
 }
