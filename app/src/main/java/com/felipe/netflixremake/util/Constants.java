a7063402b6ccf38758a549c7719a563993f4decb
Felipe <ftiagomelo89@gmail.com>
2022-04-03 10:17:40 -0300

    Migrando projeto de java para kotlin.

--- app/src/main/java/com/felipe/netflixremake/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.felipe.netflixremake.util;
-
-public class Constants {
-
-    public static final String CATEGORY_URL = "https://tiagoaguiar.co/api/netflix/home";
-    public static final String MOVIE_DETAIL_BASE_URL = "https://tiagoaguiar.co/api/netflix/";
-}
